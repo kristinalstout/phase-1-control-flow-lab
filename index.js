9ae7c68c1d02ec2930749373f14e67f5401826e7
Kristina Stout <kristinalstout@gmail.com>
2023-06-12 11:50:49 -0400

    added functions

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+let depth;
+function scuberGreetingForFeet(depth){
   // Write your code here!
+  let result;
+  if (depth <= 400) { 
+    result = "This one is on me!";
+  } else if (depth > 400 && depth <=2000) {
+    result = "That will be twenty bucks.";
+  } else if (depth > 2000 && depth < 2500){
+    result = "I will gladly take your thirty bucks.";
+  } else if (depth > 2500){
+    result = "No can do.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+let city;
+function ternaryCheckCity(city){
   // Write your code here!
+  const checkCity = city === "NYC"? "Ok, sounds good." : "No go.";
+  return checkCity;
 }
 
-function switchOnCharmFromTip(){
+let tip;
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response;
+  switch (tip){
+    case "generous":
+      response = "Thank you so much.";
+      break;
+    case "not as generous":
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye.";
+      break;
+    
+  } return response;
 }
